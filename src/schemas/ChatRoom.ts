c2fd1a8fe2d007d4b7c0956f343b52df9b68fa52
kielkow <matheuskiel@fiorifer.com.br>
2022-10-07 11:09:52 -0300

    feat: create chat room schema

---
+++ src/schemas/ChatRoom.ts
@@ -0,0 +1,25 @@
+import { model, Document, Schema } from 'mongoose';
+import { v4 as uuid } from 'uuid';
+import { User } from './User';
+
+type ChatRoom = Document & {
+    idUsers: User[];
+    idChatRoom: String;
+}
+
+const ChatRoomSchema = new Schema({
+    idUsers: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Users',
+        },
+    ],
+    idChatRoom: {
+        type: String,
+        default: uuid(),
+    },
+});
+
+const ChatRoom = model<ChatRoom>('ChatRooms', ChatRoomSchema);
+
+export { ChatRoom };
