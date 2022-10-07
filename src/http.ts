c36a253073e3090b70576eccdc9cef66f661969b
kielkow <matheuskiel@fiorifer.com.br>
2022-10-07 10:44:06 -0300

    feat: connect on mongodb

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import path from 'path';
+import mongoose from 'mongoose';
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect('mongodb://localhost/rocketsocket', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
