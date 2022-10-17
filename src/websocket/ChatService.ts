35eb9a5a2eafe31eae830ceb10975c6acc29e049
kielkow <matheuskiel@fiorifer.com.br>
2022-10-17 10:46:22 -0300

    feat: implement GetMessagesByChatRoomService on start_chat event

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -5,6 +5,7 @@ import { CreateMessageService } from "../services/CreateMessageService";
 import { CreateUserService } from "../services/CreateUserService";
 import { GetAllUsersService } from "../services/GetAllUsersService";
 import { GetChatRoomByUsersService } from "../services/GetChatRoomByUsersService";
+import { GetMessagesByChatRoomService } from "../services/GetMessagesByChatRoomService";
 import { GetUserBySocketIdService } from "../services/GetUserBySocketIdService";
 
 io.on('connect', socket => {
@@ -36,6 +37,7 @@ io.on('connect', socket => {
         const createChatRoomService = container.resolve(CreateChatRoomService);
         const getUserBySocketIdService = container.resolve(GetUserBySocketIdService);
         const getChatRoomByUsersService = container.resolve(GetChatRoomByUsersService);
+        const getMessagesByChatRoomService = container.resolve(GetMessagesByChatRoomService);
 
         const userLogged = await getUserBySocketIdService.execute(socket.id);
 
@@ -49,7 +51,9 @@ io.on('connect', socket => {
 
         socket.join(room.idChatRoom);
 
-        callback({ room });
+        const messages = await getMessagesByChatRoomService.execute(room.idChatRoom);
+
+        callback({ room, messages });
     });
 
     socket.on('message', async data => {
